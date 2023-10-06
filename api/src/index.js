03cf7d3fe72209fb30cddfb97844fd2b3e69c00f
richard <richardarthur200703@gmail.com>
2023-10-06 16:40:21 -0300

    ok

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import mercadoController from './controller/mercadoController.js'
+
+let servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+servidor.use(mercadoController);
+
+servidor.listen(process.env.PORT,
+                () => console.log('API subiu disgraça'))
